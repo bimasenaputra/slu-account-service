a4624d9983042c09a7c70d56897183d2e74d2a0e
Bimasena Putra <bimasena.putra@ui.ac.id>
2022-04-21 13:38:23 +0700

    Reset to change port

--- src/main/java/id/ac/ui/cs/advprog/accountservice/exception/ErrorStrategy.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.accountservice.exception;
-
-import org.springframework.http.HttpStatus;
-
-public abstract class ErrorStrategy {
-    public abstract String getMessage();
-    public abstract HttpStatus getStatus();
-}
