abad626781139e0923ec8236ec13166123c568d8
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2022-04-22 01:12:47 +0700

    Add no-argument constructor for AccountId

--- src/main/java/id/ac/ui/cs/advprog/accountservice/model/AccountId.java
+++ src/main/java/id/ac/ui/cs/advprog/accountservice/model/AccountId.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.accountservice.model;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@NoArgsConstructor
 @AllArgsConstructor
 public class AccountId implements Serializable {
     private String email;
